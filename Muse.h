94eea764423a496f5ab7f3ad79a17723b7ecd564
Lanyun Hou <a72@luotianyi.pro>
2018-12-25 19:43:00 +0800

    μsic 4EVER!

---
+++ Muse.h
@@ -0,0 +1,9 @@
+#define COLOUR_COUNT 9
+//Colours of Aqours group members(Tweaked for bad colour display
+// details of AdaFruit NeoPixel). :(
+const CRGB colour[] = {
+  0xE2732D,0x36B3DD,0x8C9395,0x1660A5,//Honoka,Eli,Kotori,Umi,Rin
+  0xF1C51F,0xCC3554,0x744791,0x54AB48,0xD54E8D};//Maki,Nozumi,Hanayo,Nico
+
+//Array of names that being used for 
+const String names[] = {"Honoka","Eli","Kotori","Umi","Rin","Maki","Nozumi","Hanayo","Nico"};
