51c0323036ccaf2e731eadbfb55b622cdb81aab5
Lanyun Hou <a72@luotianyi.pro>
2018-12-25 20:38:23 +0800

    Waiting for official color release

---
+++ PERFECT_Dream.h
@@ -0,0 +1,7 @@
+#define COLOUR_COUNT 1
+//We are waiting for official colours that will be released by the project.
+//While we are waiting, you can create your very own colour group!
+const CRGB colour[] = {0xFFFFFF};
+
+//Array of names that being used for 
+const String names[] = {"Comming soon"};
